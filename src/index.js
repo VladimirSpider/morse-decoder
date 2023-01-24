c06dfbbc2c3c39fd6d73a2422a9d3f7a04368d6e
VladimirSpider <vovchikbura@mail.ru>
2023-01-24 21:21:46 +0300

    feat: created function decode

--- src/index.js
+++ src/index.js
@@ -39,6 +39,64 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const lettersArray = [];
+    const finalLettersArray = [];
+
+    const getLettersArray = (string) => {
+        const arraySymbols = string.split('');
+
+        let startIndex = 0;
+        let endIndex = 10;
+        while (endIndex <= arraySymbols.length) {
+            const letterArray = arraySymbols.slice(startIndex, endIndex);
+            startIndex += 10;
+            endIndex += 10;
+            lettersArray.push(letterArray);
+        }
+    }
+
+    getLettersArray(expr);
+
+    const newArray = lettersArray.map((letterSymbols) => {
+        const morseLetter = letterSymbols.filter((item, index) => {
+            if (item === '*') return item;
+
+            if (item === '1') return item;
+
+            if ((item === '0' && index !== 0 && ((letterSymbols[index - 1] !== '0' && letterSymbols[index + 1] !== '0') || index === letterSymbols.length - 1))) {
+                return item;
+            }
+        });
+
+        const readyMorseLetter = morseLetter.map((item, index) => {
+            if (item === '*') return item;
+
+            if (index !==0 && index % 2 !== 0) {
+                return morseLetter[index - 1] + item;
+            }
+        });
+
+        const endReadyMorseLetter = readyMorseLetter.filter((item) => !!item);
+
+        const finalArray = endReadyMorseLetter.map((item) => {
+            if (item === '*') return item;
+            if (item === '10') return '.';
+            if (item === '11') return '-';
+        });
+
+        return finalArray.join('');
+    });
+
+    newArray.forEach((item) => {
+        if (item === '**********') {
+            finalLettersArray.push(' ');
+        } else {
+            finalLettersArray.push(MORSE_TABLE[item]);
+        };
+    });
+
+    return finalLettersArray.join('');
+
 }
 
 module.exports = {
